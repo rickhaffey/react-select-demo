d05d71ac1f3af82598fe2142283ef17859cc4187
Rick Haffey <rhaffey@fanatics.com>
2017-11-14 20:56:19 -0600

    basic select functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import people from './peopledata/index.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      "selectedId": undefined
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      "selectedId": event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <select onChange={this.handleChange}>
+        {
+          people.map((person, index) => <option key={index} value={person.id}>{person.name}</option>)
+        }
+        </select>
+
+        <div className={ this.state.selectedId ? "result" : "no-result" } >
+            The selected person has id: <b>{this.state.selectedId}</b>.
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/peopledata/index.js
@@ -0,0 +1,10 @@
+export default [
+		{ "name": "Adam", "id": 1 },
+		{ "name": "Beth", "id": 2 },
+		{ "name": "Charles", "id": 3 },
+		{ "name": "Dianne", "id": 4 },
+		{ "name": "Edward", "id": 5 },
+		{ "name": "Faith", "id": 6 },
+		{ "name": "George", "id": 7 },
+		{ "name": "Holly", "id": 8 }
+	]
